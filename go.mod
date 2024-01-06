aaf6b57f6fa682aa4ba2785de768ad8e8c3b4da1
justin <justin.t.connolly@gmail.com>
2024-01-06 17:58:53 -0600

    add rate limiting

--- go.mod
+++ go.mod
@@ -6,3 +6,8 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 )
+
+require (
+	github.com/juju/ratelimit v1.0.2
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
--- main.go
+++ main.go
@@ -43,6 +43,11 @@ func main() {
 
 	//api logic will go here and utilize the database connection called dbConn
 	http.HandleFunc("/api/v1/pokemon/", func(w http.ResponseWriter, r *http.Request) {
+		if bucket.TakeAvailable(1) != 1 {
+			http.Error(w, "Too many requests", http.StatusTooManyRequests)
+			return
+		}
+
 		validateAPIKey(dbConn, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			handlers.GetPokemonByName(dbConn, w, r)
 		})).ServeHTTP(w, r)
